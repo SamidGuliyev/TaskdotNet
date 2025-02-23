e7b56c672488bb7652bec0ad5e9bc9aa7dad8b87
SamidGuliyev <samid1036@gmail.com>
2025-02-23 10:28:59 +0400

    First commit dotnet

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿int[] arr = [1, 3, 5, 7, 9,];
+
+List<int> list = [];
+
+for (int i = 0; i < arr.Length; i++)
+{
+
+    var result = 0;
+
+
+    for (int j = 0; j < arr.Length; j++)
+    {
+
+        if (i != j)
+        {
+            result += arr[j];
+
+        }
+
+
+    }
+
+    list.Add(result);
+
+
+}
+
+var max = list.Max();
+var min = list.Min();
+Console.WriteLine(min.ToString() + " " + max.ToString());
